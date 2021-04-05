9268ff36fa0f63fc7a421d45da95b916cdd2c766
gcostaa <gustavokc05061999@gmail.com>
2021-04-05 08:39:13 -0300

    Propriedades

--- Python/OO/OO2/MOD1/modelo.py
+++ Python/OO/OO2/MOD1/modelo.py
@@ -5,28 +5,65 @@ class Filme:
     def __init__(self,nome,ano,duracao):
 
         #title aumenta letra de cada palavra
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
 
     def dar_like(self):
-        self.likes +=1
+        self.__likes +=1
 
+    @property
+    def likes(self):
+
+        return self.__likes
+
+    @property
+    def nome(self):
+
+        return self.__nome
+
+    #para definir um set como propriedades
+
+    @nome.setter
+
+    def nome(self,novo_nome):
+
+        self.__nome = novo_nome
 class Serie:
 
     def __init__(self,nome,ano,temp):
 
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.temp = temp
-        self.likes = 0
+        self.__likes = 0
 
     def dar_like(self):
         self.likes +=1
+    
+    @property
+    def likes(self):
+
+        return self.__likes
+
+    @property
+    def nome(self):
+
+        return self.__nome
+
+    #para definir um set como propriedades
+
+    @nome.setter
+
+    def nome(self,novo_nome):
+
+        self.__nome = novo_nome
 
 filme = Filme("Vingadores",2018,120)
 filme.dar_like()
+
+#com as propriedade aqui conseguimos acessar o atributo de forma mais facil
 print (f"Nome: {filme.nome} - Likes: {filme.likes}")
 
 serie = Serie("Teste",2018,120)
