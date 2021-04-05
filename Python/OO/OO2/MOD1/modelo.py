9840f8359e623da8398a3bb92a943997636bd189
gcostaa <gustavokc05061999@gmail.com>
2021-04-05 08:29:03 -0300

    implementando likes

--- Python/OO/OO2/MOD1/modelo.py
+++ Python/OO/OO2/MOD1/modelo.py
@@ -4,18 +4,30 @@ class Filme:
 
     def __init__(self,nome,ano,duracao):
 
-        self.nome = nome
+        #title aumenta letra de cada palavra
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
 
+    def dar_like(self):
+        self.likes +=1
 
 class Serie:
 
     def __init__(self,nome,ano,temp):
 
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temp = temp
+        self.likes = 0
 
-vingadores = Filme("Vingadores",2018,120)
-print (f"Nome: {vingadores.nome}")
+    def dar_like(self):
+        self.likes +=1
+
+filme = Filme("Vingadores",2018,120)
+filme.dar_like()
+print (f"Nome: {filme.nome} - Likes: {filme.likes}")
+
+serie = Serie("Teste",2018,120)
+print (f"Nome: {serie.nome} - Likes: {serie.likes}")
